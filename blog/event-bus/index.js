da841e8efcdde6f602729dcdc7f7d4adac27269f
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2020-08-09 18:03:53 +0300

    event syncing

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,10 +5,13 @@ import axios from 'axios';
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 
 app.post('/events', (req, res) => {
   const event = req.body;
   
+  events.push(event);
+
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
@@ -16,6 +19,10 @@ app.post('/events', (req, res) => {
 
   res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log('Events-bus listening on port: 4005');
