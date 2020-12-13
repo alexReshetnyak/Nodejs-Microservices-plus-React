cae584fa333896c4ea5bf98e699fb6f758e99414
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2020-12-13 19:55:50 +0200

    Update client app: implement sign up logic

--- ticketing/client/pages/index.jsx
+++ ticketing/client/pages/index.jsx
@@ -1,5 +1,19 @@
-const App = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('I am in the component', currentUser);
+
+  // axios.get('/api/users/currentuser');
+
   return <h1>Landing page</h1>;
 };
 
-export default App;
+LandingPage.getInitialProps = async () => {
+  console.log('I am on the server!');
+
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
