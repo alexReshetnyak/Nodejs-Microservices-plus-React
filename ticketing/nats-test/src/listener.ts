4ba2cdb2bd0aeb45d9f5cd99d3adabcef3f62bdd
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2021-02-16 10:06:31 +0200

    test-nats: setManualAckMode to true

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,15 +1,25 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const clusterId = 'ticketing';
+const randomClientId = randomBytes(4).toString('hex');
+const stan = nats.connect(clusterId, randomClientId, {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to nats');
 
-  const subscription = stan.subscribe('ticket:created');
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const queueGroupName = 'orders-service-queue-group';
+  const subscription = stan.subscribe(
+    'ticket:created',
+    queueGroupName,
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -19,6 +29,11 @@ stan.on('connect', () => {
         `Received event #${msg.getSequence()}, with data: ${JSON.parse(data)}`
       );
     }
+
     console.log('MESSAGE RECEIVED', msg.getSequence(), data);
+
+    // * Let the nats know (acknowledge) that the event was successfully processed
+    // * If you don't, nats will wait 30 seconds and try to send the event again.
+    msg.ack();
   });
 });
