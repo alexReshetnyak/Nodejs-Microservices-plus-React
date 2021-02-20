6cfb648862f3931e80ea5efd7b4aa8efdee70fc8
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2021-02-20 18:34:40 +0200

    create abstract class Listener

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan, SubscriptionOptions } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -17,11 +17,15 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const queueGroupName = 'orders-service-queue-group';
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable() // * Send all not processed history events when listener starts
+    .setDurableName('accounting-service');
+
   const subscription = stan.subscribe(
     'ticket:created',
-    queueGroupName,
+    'queue-group-name', // * Set group name to avoid events history cleaning when listener restarts
     options
   );
 
@@ -45,3 +49,49 @@ stan.on('connect', () => {
 // * Stop client when node js execution has been interrupted or terminated
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, message: Message): void;
+
+  protected ackWait = 5 * 1000; // * 5 sec
+
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  protected listen(): void {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  private parseMessage(msg: Message): any {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+
+  private subscriptionOptions(): SubscriptionOptions {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable() // * Send all not processed history events when listener starts
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait) // * wait for 5 sec before repeat failed event
+      .setDurableName(this.queueGroupName);
+  }
+}
