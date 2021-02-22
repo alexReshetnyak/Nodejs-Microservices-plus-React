4f66398e8e670843bce4b98ac4704a02c05e70ae
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2021-02-22 09:59:35 +0200

    add nats to ticketing service

--- ticketing/README.md
+++ ticketing/README.md
@@ -189,3 +189,4 @@ http://alexey-release-ingress-nginx-controller.default.svc.cluster.local/api/use
 - `npm run publish`
 - `npm run listen`
 - Execute `kubectl port-forward nats-depl-58c56c67b-hckqk 8222:8222` to get access to http://localhost:8222/streaming or http://localhost:8222/streaming/channelsz?subs=1
+- `kubectl delete pod nats-depl-58c56c67b-hckqk`
--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,12 +1,27 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats.wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
 
+  // * Connect to NATS
+  const clusterId = 'ticketing'; // got from nats-depl.yaml
+  const natsUrl = 'http://nats-srv:4222'; // based on nats-depl.yaml settings
+
+  await natsWrapper.connect(clusterId, 'client-id-random', natsUrl);
+
+  natsWrapper.client.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+  process.on('SIGINT', () => natsWrapper.client.close());
+  process.on('SIGTERM', () => natsWrapper.client.close());
+
+  // * Connect to MONGO DB
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
