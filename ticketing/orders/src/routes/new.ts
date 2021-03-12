bc3178f71fd443eb65681b0bf4f05f006b56d2fa
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2021-03-12 10:08:58 +0200

    implement endpoint to create new order

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@alexey-corp/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@alexey-corp/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
 
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60; // 15 minutes
 
 router.post(
   '/api/orders/',
@@ -13,13 +23,38 @@ router.post(
       .not()
       .isEmpty()
       .custom((input: string) => {
-        mongoose.Types.ObjectId.isValid(input);
+        return mongoose.Types.ObjectId.isValid(input);
       })
       .withMessage('Ticket ID must be provided'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send(order);
   }
 );
 
