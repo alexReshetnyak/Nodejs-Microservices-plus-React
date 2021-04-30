7b03dfa842bbcfd3eb06c6bca174488f4bd27d3c
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2021-04-30 10:00:52 +0300

    order service: add expiration complete publisher and listener

--- ticketing/expiration/src/queues/expiration-queue.ts
+++ ticketing/expiration/src/queues/expiration-queue.ts
@@ -1,5 +1,8 @@
 import Queue from 'bull';
 
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats.wrapper';
+
 interface Payload {
   orderId: string;
 }
@@ -11,11 +14,10 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
   },
 });
 
-expirationQueue.process(async (job) => {
-  console.log(
-    'I want to publish an expiration:complete event for orderId',
-    job.data.orderId
-  );
+expirationQueue.process(async (job: Queue.Job<Payload>) => {
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 });
 
 export { expirationQueue };
